b4b47b8a34814280d38eb9c081e384495db4940f
PetrosaMan <dw.todd.eng@gmail.com>
2024-10-20 21:32:03 +0200

    Changed code to use random colors in squares

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Etch-a-Sketch
 Create a web page with a 16 x 16 grid of squares
+
+The mouseover event listener is inside the for loop because we want to attach the event handler to each individual square as they’re created. Each grid square needs its own listener so it can respond when the mouse hovers over it.
+
+If we put the listener outside the loop, it wouldn't apply to the squares since they haven't been created yet. Think of the loop as setting up each square with its own behavior as it’s being added to the grid.
--- script.js
+++ script.js
@@ -18,24 +18,29 @@ function drawGrid(squares) {
         const square = document.createElement("div");        
         square.classList.add("square");               
         square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        
-        // add mouseover event to change color
+        square.style.height = `${squareSize}px`; 
+
+        // add mouseover event to randomly change square
+        // background on mouseover
         square.addEventListener('mouseover', function(){
-          square.classList.add('hovered');
+            square.classList.add('hovered');
+            const r = parseInt(Math.random() * 255);
+            const g = parseInt(Math.random() * 255);
+            const b = parseInt(Math.random() * 255);
+            square.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b  +')';            
         });
+        
         gridContainer.appendChild(square);
     }
 }
 
+
+
 btn.addEventListener("click", function() {
-    let squares = parseInt(prompt("Enter grid squares per side required", '0'));    
+    let squares = parseInt(prompt("Enter grid squares per side required. Maximum 100 squares", '0'));    
     if(squares === 0 || isNaN(squares)) {
          alert( "Invalid entry, try again");
-         return;
-    /*} else if (isNaN(squares)) {
-        alert  ('Invalid input, try again');
-        return;*/
+         return;    
     }
     drawGrid(squares);    
 });
